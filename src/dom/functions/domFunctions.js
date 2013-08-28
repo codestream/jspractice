092b10a09a68896eea638edf6d2e2f6978384100
Viacheslav Okolitiy <justforgit@gmail.com>
2013-08-28 15:49:06 +0300

    fixed remove class, added check to addClass

--- src/dom/functions/domFunctions.js
+++ src/dom/functions/domFunctions.js
@@ -2,16 +2,36 @@
 
 window.onload = function () {
     var div = document.createElement("div");
-    div.className = "text red test red";
+    div.className = "lorem ipsum dolor sit amet consectetur adipisicing lorem ipsum";
     div.innerHTML = "It is test div";
+    div.setAttribute("id", "testdiv");
     document.body.appendChild(div);
 
     function removeClass(node, className) {
-        var classNode = node.className;
-        var regex = new RegExp("\\s?\\b" + className + "\\b", "g");
-        classNode = classNode.replace(regex, '');
-        node.className = classNode;
+        var element = document.getElementById(node.id);
+        element.classList.remove(className);
     }
 
-    removeClass(div, "red");
+    function addClass(node, className) {
+        if (className !== "") {
+            var classNode = node.className;
+
+            if (classNode.match(className)) {
+                return;
+            }
+
+            if (classNode != '') {
+                className = ' ' + className;
+            }
+
+            node.className = classNode + className;
+        }
+
+    }
+
+    addClass(div, "red-label"); //correct
+    addClass(div, "lorem"); //can't add
+    removeClass(div, "lorem"); //removed all classes with className lorem
+
+    console.log(div.className);
 };
